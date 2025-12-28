4e5c1b790c8b81d35c8fd30a543f26adba8393c8
YassineSdk <yassine.sadiki2-etu@etu.univh2c.ma>
2025-12-28 19:24:04 +0100

    updated the connect_function

--- src/connect_db.py
+++ src/connect_db.py
@@ -6,19 +6,19 @@ import os
 
 def connect_db():
     """
-    this function is for the conenction with the database 
+    this function is for the connection with the database 
     """
-    load_dotenv("/home/ubuntu/projects/Housing_Data_API/.env")
+    load_dotenv("/home/yassine/projects/Housing_Data_API/.env")
     conn_string = os.getenv("URL_CONN")
-    
-    try :
-        engine = create_engine(str(conn_string),
+
+    engine = create_engine(str(conn_string),
                             pool_pre_ping=True,
                             pool_size=50,
                             max_overflow=100,
                             future=True)
-    except Exception as e :
-        print(f"connection failed : {e}")
-    return engine
-
+    if engine:
+        print("engine is ready")
+        return engine
+    else : 
+        print("having error loading the engine")
 
