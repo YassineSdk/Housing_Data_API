dee825474b5f52d5d0ff1d516fe70d1cdbea0677
YassineSdk <yassine.sadiki2-etu@etu.univh2c.ma>
2026-01-09 14:52:25 +0100

    testing

--- src/connect_db.py
+++ src/connect_db.py
@@ -8,7 +8,8 @@ def connect_db():
     """
     this function is for the connection with the database 
     """
-    conn_string = os.getenv("URL_CONN")
+    #conn_string = os.getenv("URL_CONN")
+    conn_string = "postgresql://neondb_owner:npg_jHrY0GKz9AWB@ep-winter-tree-afw0h8g6-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
     if not conn_string : 
         raise RuntimeError("URL_CONN environment variable is not set")
